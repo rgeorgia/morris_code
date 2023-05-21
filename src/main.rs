f6eade8082b4b0d908add33dcedc6ad0385db42b
Ron Georgia <rong1611@gmail.com>
2023-05-21 19:48:49 -0400

    Added build_code... works now

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,46 @@
 /*
 Sentence or Word Mode (default):
 
-    - Type a sentence or a word. Enter causes morris code print out.
-    - Prints two lines. Top line is the message, second line is morris code
-    - Each word is lined up with corresponding code
-    - End of line is CR/LF
+    Type a sentence or a word. Enter causes morris code print out.
+    Prints two lines. Top line is the message, second line is morris code
+    Each word is lined up with corresponding code
+    End of line is CR/LF
 
 Character mode:
-- -c
-- Interactive. As soon as the key is pressed the code is printed.
-- CR/LF exits program
+--interactive
+Interactive. As soon as the key is pressed the code is printed.
+CR/LF exits program
 */
 
 pub mod models;
 
 use crate::models::morris_code::morris_code_table;
+use clap::Parser;
+
+/// Get the string to convert to code
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// string to convert
+    code: Option<String>,
+}
 
 fn main() {
-    let morris_table = morris_code_table();
+    let args = Args::parse();
 
-    println!("Quote is {}", morris_table.get("\"").unwrap());
-    println!("Capitable A is {}", morris_table.get("A").unwrap());
+    let input = args.code;
+
+    println!("{}", build_code(&input.unwrap()));
+}
+
+fn build_code(input_str: &str) -> String {
+    let morris_table = morris_code_table();
+    let mut result: String = String::new();
+
+    for item in input_str.chars() {
+        result.push_str(morris_table.get(&item.to_string().to_uppercase()).unwrap());
+    }
+    result
 }
 
 #[cfg(test)]
